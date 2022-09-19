994fb1a097d2fa90ee79bb7c3cdde579c19a2974
Lonchakova Anastasiya <aalonchakova@gmail.com>
2022-09-19 22:52:27 +0300

    Расчет бонусных миль

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 100;
+        int programCondition = 20;
+        int miles = ticketPrice / programCondition;
+
+        System.out.println(miles);
+    }
+}
